ab5dc4e8da72a5bc209c6102d36841403d388abc
Vatsal Patel <vatsalnew61@gmail.com>
2022-06-09 02:29:40 -0400

    sysinfo.sh

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+echo Report from myvm 
+echo ================
+
 echo "Host Name: $HOSTNAME"
 
+
+echo Operating System name and version: $(uname -v) / $(uname -o)
+
+
 z=https://zonzorp.github.io/COMP2101/GCFlex/Labs/bash/COMP2101-bash-lab1.html
+echo FQDN:
 echo $Domain Name= "$z" | awk -F/ '{print $3}'
 
-#Operating system Name and version:
 
-egrep '^(VERSION|NAME)=' /etc/os-release
-
-echo "Ip Addresses="
-d= ip addr show ens33 # Ip address
+echo "IP Address: `hostname -I`"
  
-echo "Root file system status:"
-df /
+echo Root Filesystem Free Space:$(du -h)
+echo ================
